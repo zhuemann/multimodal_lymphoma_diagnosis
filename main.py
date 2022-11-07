03dcc6ec44027186cb1e20a8d9a9c1ba368774d6
zhuemann <56049645+zhuemann@users.noreply.github.com>
2022-11-07 11:03:45 -0600

    run da roberta

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ if __name__ == '__main__':
         ## save to xlsx file
         #filepath = os.path.join(directory_base, '/UserData/Zach_Analysis/result_logs/for_abstract/bio_clinical_bert/confusion_matrix_seed' + str(seed) + '.xlsx')
         filepath = os.path.join(directory_base,
-                                '/UserData/Zach_Analysis/result_logs/for_paper/paper_workspace/roberta_base_5e6_lr_v18/confusion_matrix_seed' + str(
+                                '/UserData/Zach_Analysis/result_logs/for_paper/paper_workspace/roberta_base_5e6_lr_v19/confusion_matrix_seed' + str(
                                     seed) + '.xlsx')
         df.to_excel(filepath, index=False)
 
