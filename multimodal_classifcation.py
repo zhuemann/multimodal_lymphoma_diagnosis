c9fccb85669d02608e3144768fc29ef6f3a0d76e
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-01-21 12:44:35 -0600

    updating model links

--- multimodal_classifcation.py
+++ multimodal_classifcation.py
@@ -313,7 +313,7 @@ def multimodal_classification(seed, batch_size=8, epoch=1, dir_base = "/home/zmh
     #IMG_SIZE = 224
     IMG_SIZE = 384
     BATCH_SIZE = batch_size
-    LR = 5e-6 #5e-6 #5e-6 best#1e-06 #2e-6
+    LR = 1e-5 #5e-6 #5e-6 #5e-6 best#1e-06 #2e-6
     GAMMA = 0.7
     N_EPOCHS = epoch #8
     N_CLASS = n_classes
@@ -346,8 +346,8 @@ def multimodal_classification(seed, batch_size=8, epoch=1, dir_base = "/home/zmh
     # using bert for now
     #language_path = os.path.join(dir_base, 'Zach_Analysis/models/bert/')
     #language_path = os.path.join(dir_base, 'Zach_Analysis/models/bio_clinical_bert/')
-    #language_path = os.path.join(dir_base, 'Zach_Analysis/models/rad_bert_pretrained_v4/')
-    language_path = os.path.join(dir_base, 'Zach_Analysis/models/rad_bert/')
+    language_path = os.path.join(dir_base, 'Zach_Analysis/models/rad_bert_pretrained_v4/')
+    #language_path = os.path.join(dir_base, 'Zach_Analysis/models/rad_bert/')
     #language_path = os.path.join(dir_base, 'Zach_Analysis/models/roberta_large_pretrained_recreated/')
     #language_path = os.path.join(dir_base, 'Zach_Analysis/roberta_large/')
 
