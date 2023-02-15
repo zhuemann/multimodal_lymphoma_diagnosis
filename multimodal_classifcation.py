ce42108e6077c98c6f015bdc02a54a51aeb5a580
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-02-15 13:50:15 -0600

    human comparisoin

--- multimodal_classifcation.py
+++ multimodal_classifcation.py
@@ -275,6 +275,7 @@ class TextImageDataset(Dataset):
         # images data extraction
         img_name = self.row_ids[index]
         img_name = str(img_name) + "_mip.png"
+        print(self.data_path)
         if exists(os.path.join(self.data_path, 'Group_1_2_3_curated', img_name)):
             data_dir = "Group_1_2_3_curated"
         if exists(os.path.join(self.data_path, 'Group_4_5_curated', img_name)):
