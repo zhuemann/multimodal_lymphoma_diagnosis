0e0fbb702ae0f34af6059e8fe2172de9270930fb
zhuemann <56049645+zhuemann@users.noreply.github.com>
2023-02-17 13:32:22 -0600

    human comparisoin

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ if __name__ == '__main__':
     #seeds = [712]
     #seeds = [1555, 1779, 2001, 2431, 2897, 3194, 4987, 5693 ,6386]
     #seeds = [117,295,98,456,915,1367,712]
-    seeds = [456, 712, 915, 1367]
+    seeds = [98, 117, 295, 456, 712, 915, 1367]
     accuracy_list = []
     for seed in seeds:
         #filepath = 'Z:/Zach_Analysis/result_logs/confusion_matrix_seed' + str(seed) + '.xlsx'
@@ -81,7 +81,7 @@ if __name__ == '__main__':
         ## save to xlsx file
         #filepath = os.path.join(directory_base, '/UserData/Zach_Analysis/result_logs/for_abstract/bio_clinical_bert/confusion_matrix_seed' + str(seed) + '.xlsx')
         filepath = os.path.join(directory_base,
-                                '/UserData/Zach_Analysis/result_logs/for_paper/paper_workspace/roberta_ai_vs_human_comparison_v35/confusion_matrix_seed' + str(
+                                '/UserData/Zach_Analysis/result_logs/for_paper/paper_workspace/roberta_ai_vs_human_comparison_v36/confusion_matrix_seed' + str(
                                     seed) + '.xlsx')
         df.to_excel(filepath, index=False)
 
